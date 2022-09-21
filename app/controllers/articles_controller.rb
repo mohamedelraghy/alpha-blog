dec9c9b0d406cced7168f24290799abb4a423bb8
mohamed El-Raghy <elraghy8@gmail.com>
2022-09-21 19:47:49 +0200

    add navbar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
     
         @article.destroy
         flash[:notice] = "Article Deleted successfully"
-        redirect_to articles_path
+        redirect_to articles_path, status: :see_other
     end
 
     private
