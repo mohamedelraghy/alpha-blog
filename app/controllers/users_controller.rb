6d1421e2cf6777786d7d3c7d2fac47cd61a202ad
mohamed El-Raghy <elraghy8@gmail.com>
2022-10-25 15:36:11 +0200

    add user functionalty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -39,6 +40,12 @@ class UsersController < ApplicationController
 
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] = "User and all articles created by user have been deleted"
+        redirect_to users_path
+    end
     private
     def user_params 
         params.require(:user).permit(:username, :email, :password)
@@ -49,8 +56,15 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-        if !loggin_in? && current_user != @user
+        if  current_user != @user and !current_user.admin?
             flash[:danger] = 'You can only edit you account'
+            redirect_to root_path
+        end
+    end
+
+    def require_admin 
+        if logged_in? and !current_user.admin?
+            flash[:danger] = 'Only admin users can perform that action'
             redirect_to root_path
         end
     end
