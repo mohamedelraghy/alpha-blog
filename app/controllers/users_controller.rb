9c81b6c282b474217c52024ece35aa43fb054577
mohamed El-Raghy <elraghy8@gmail.com>
2022-10-25 15:41:58 +0200

    fix typo in delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @user.destroy
         flash[:danger] = "User and all articles created by user have been deleted"
-        redirect_to users_path
+        redirect_to users_path, status: :see_other 
     end
     private
     def user_params 
